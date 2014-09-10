f58288630aca2e0f8929e64f682319f204043bf4
Alexander Mendez <bluespace@gmail.com>
2014-09-09 17:38:51 -0700

    update for archive

--- casperjs/archive.js
+++ casperjs/archive.js
@@ -1,49 +1,23 @@
-
-
-var page = require('webpage').create(),
-    system = require('system'),
-    t, address;
-
+// casper
+// Mendez 2014
 var casper = require('casper').create();
 var fs = require('fs')
-var pw = fs.read('/Users/ajmendez/.limited/comcast.pw');
-var bwlog = '/Users/ajmendez/data/bandwidth/archive.txt';
-
-
-address = 'https://customer.comcast.com/MyServices/Internet/?ajax=1';
-t = Date.now();
+var pw = fs.read("/home/ajmendez/.limited/comcast.pw");
+var user = fs.read("/home/ajmendez/.limited/comcast.user");
+var bwlog = "/home/ajmendez/data/bandwidth/archive.txt";
+var address = 'https://customer.comcast.com/MyServices/Internet/?ajax=1';
 
 casper.start(address, function() {
-    // console.log('title: ' + this.getTitle());
     this.fill('form[name="signin"]', {
-        'user': 'blue.space2014',
+        'user': user,
         'passwd':pw,
     }, true);
-    // this.click('form[name="signin"]').submit();
-     // this.capture('login.png');
 });
 
 casper.then(function() {
-    // this.capture('then.png');
-    // this.echo(this.getTitle());
-    fs.write(bwlog, Date.now()+' : '+this.getHTML('span[class="cui-meter-details-info"]'), 'a');
-    // this.evaluateOrDie(function() {
-    //     console.log(this.getTitle());
-    //     return /'t'/.test(document.body.innerText);
-    // }, 'Failed to login');
+    fs.write(bwlog, Date.now()+' : '+this.getHTML('span[class="cui-meter-details-info"]')+'\n', 'a');
 });
 
 casper.run(function () {
     this.echo('message sent').exit();
 });
-
-
-// page.open(address, function(status) {
-//     console.log(t + ': '+ status);
-//     // page.render('comcast.png');
-//     var title = page.evaluate(function() {
-//         return document.title;
-//     })
-//     console.log('title: '+title);
-//     phantom.exit();
-// });
