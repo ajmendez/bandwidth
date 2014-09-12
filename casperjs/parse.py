c67138869fec0c718e73b1f814ad2b7c366a4a0f
Alexander Mendez <bluespace@gmail.com>
2014-09-12 16:15:35 -0700

    working on parse plot

--- casperjs/parse.py
+++ casperjs/parse.py
@@ -4,6 +4,7 @@ import sys
 import matplotlib
 matplotlib.use('agg')
 import pylab
+import numpy as np
 from datetime import datetime
 from pysurvey.plot import setup, dateticks
 
@@ -12,16 +13,20 @@ PLOT = ('plot' in sys.argv)
 def parse():
     with open(FILENAME, 'r') as f:
         data = f.readlines()
-    times = [datetime.fromtimestamp(int(x.split(':')[0].strip())/1000.0) for x in data]
-    values = [int(x.split(':')[1].split('G')[0].strip()) for x in data]
+    times = np.array([datetime.fromtimestamp(int(x.split(':')[0].strip())/1000.0) for x in data])
+    values = np.array([int(x.split(':')[1].split('G')[0].strip()) for x in data])
     print 'Last Time: {} Value: {}'.format(datetime.now() - times[-1],values[-1])
     
     if PLOT:
-        setup(figsize=(6,6))
+        setup(figsize=(12,6), subplt=(1,2,1))
         pylab.plot(times,values)
-        dateticks('%Y.%M.%d')
+        dateticks('%Y.%m.%d')
+        
+        setup(subplt=(1,2,2))
+        pylab.plot(times[:-1],np.diff(values), 'sk')
+        dateticks('%Y.%m.%d')
+        
         pylab.savefig('values.png')
-    # for t,v in zip(times,values): print t,v
 
 if __name__ == '__main__':
     parse()
