2d89f917c8880ba059d5e25097c76e188c13210b
Alexander Mendez <bluespace@gmail.com>
2014-10-30 00:26:05 -0400

    working on plot

--- comcast/parse.py
+++ comcast/parse.py
@@ -10,6 +10,7 @@ from pysurvey.plot import setup, dateticks
 from matplotlib.dates import date2num
 
 FILENAME = os.path.expanduser('~/data/bandwidth/archive.txt')
+OUTFILE = os.path.expanduser('~/data/bandwidth/home.png')
 PLOT = ('plot' in sys.argv)
 def parse():
     with open(FILENAME, 'r') as f:
@@ -24,33 +25,49 @@ def parse():
         setup(figsize=(8,8))
         
         setup(subplt=(2,2,1), subtitle='Monthly Integrated',
-              ylog=True, ylabel='Number', yr=[1e-1, 2e3])
+              ylabel='Cumulative GB per month',
+              # ylog=True, yr=[1e-1, 2e3]
+              )
         pylab.plot(times,values)
         pylab.plot(times[:-1][ii], x[ii], '.k', linewidth=0)
         dateticks('%m.%d')
         
-        setup(subplt=(2,2,3), subtitle='by week')
-        tmp = (date2num(times[:-1]) % 7)
-        pylab.hist(tmp, bins=np.arange(0,7,1), alpha=0.75, linewidth=0)
+        
+        tmp = date2num(times[:-1][ii])
+        setup(subplt=(2,2,2), subtitle='Daily Delta', xlabel='Date', ylabel='Daily GB',
+              xr=[np.min(tmp), np.max(tmp)])
+        days = np.ceil(np.max(tmp) - np.min(tmp))
+        pylab.hist(tmp, bins=days, weights=x[ii],
+                   alpha=0.75, linewidth=0.2, edgecolor='w')
+        dateticks('%m.%d')
         
         
-        setup(subplt=(2,2,2), xticks=True, subtitle='By hour of the day')
+        setup(subplt=(2,2,3), subtitle='Week Delta', 
+              xlabel='Day of the week', ylabel='GB / day of week',
+              xtickv=np.arange(7)+0.5, xr=[0,7],
+              xticknames=['sun','mon','tue','wed','thur','fri','sat']
+              )
+        tmp = (date2num(times[:-1]) % 7.0)
+        pylab.hist(tmp[ii], bins=np.arange(0,8,0.25), weights=x[ii], 
+                   alpha=0.75, linewidth=0.2, edgecolor='w')
+        
+        
+        setup(subplt=(2,2,4), subtitle='Hour Delta',
+              xlabel='Hour of the Day', ylabel='GB / hour of day',
+              xr=[0,24],)
         tmp = (date2num(times[:-1]) % 1)*24.0
         pylab.hist(tmp[ii], bins=np.arange(0,25,1), weights=x[ii],
-                   alpha=0.75, linewidth=0.1, edgecolor='w')
+                   alpha=0.75, linewidth=0.2, edgecolor='w')
         # pylab.plot(tmp, np.diff(values), 'sk')
         # dateticks('%Y.%m.%d')
         
         
         
-        setup(subplt=(2,2,4), subtitle='By Day')
-        days = np.ceil(np.max(date2num(times[:-1][ii])) - np.min(date2num(times[:-1][ii])))
-        pylab.hist(date2num(times[:-1][ii]), bins=days, weights=x[ii],
-                   alpha=0.75, linewidth=0)
-        dateticks('%m.%d')
+        
+        
         pylab.tight_layout()
-        pylab.show()
-        pylab.savefig(os.path.expanduser('~/data/bandwidth/values.png'))
+        # pylab.show()
+        pylab.savefig(OUTFILE)
 
 if __name__ == '__main__':
     from pysurvey.util import setup_stop
