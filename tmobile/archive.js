11d4619e3e0b6718194cda1ff621895daaca545e
Alexander Mendez <bluespace@gmail.com>
2014-12-20 13:28:31 -0800

    tmobile

--- tmobile/archive.js
+++ tmobile/archive.js
@@ -1,6 +1,17 @@
 // casper
 // Mendez 2014
 var casper = require('casper').create();
+casper.options.viewportSize = {width: 600, height: 600};
+casper.pageSettings = {javascriptEnabled: true, loadImages:  true, loadPlugins: true};
+phantom.cookiesEnabled = true;
+
+
+casper.echo("Casper CLI passed args:");
+require("utils").dump(casper.cli.args);
+
+casper.echo("Casper CLI passed options:");
+require("utils").dump(casper.cli.options);
+
 // casper.on('resource.requested', function(resource) {
 //
 //     for (var obj in resource.headers) {
@@ -12,9 +23,52 @@ var casper = require('casper').create();
 //     }
 //
 // });
-casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36');
-// casper.options.verbose=true;
-// casper.options.logLevel='debug';
+// casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36');
+
+casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
+
+// casper.on('resource.requested', function(resource) {
+//     casper.echo(resource.url);
+// });
+casper.on('resource.received', function(resource) {
+    casper.echo(' Loaded: '+ resource.url);
+});
+
+// casper.on('page.resource.requested', function(requestData, request) {
+//     if (requestData.url.indexOf('https://www.t-mobile.com/foresee/') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://nexus.ensighten.com/') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://www.googleadservices.com/') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://googleads.g.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://fls.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://*.fls.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://fls.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://fls.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+//     if (requestData.url.indexOf('https://fls.doubleclick.net') === 0) {
+//         request.abort();
+//     }
+// });
+
+
+
+casper.options.verbose=true;
+casper.options.logLevel='debug';
+
 var fs = require('fs')
 
 var getDir = function() {
@@ -33,35 +87,74 @@ var user = tmp.split(' ')[0];
 var pw = tmp.split(' ')[1];
 var bwlog = dir+"/data/bandwidth/tmobile.txt";
 var address = 'https://my.t-mobile.com/login/Default.aspx';
-
+var address = 'https://my.t-mobile.com/Default.aspx';
+var address = 'https://my.t-mobile.com/Plan/Prepaid/Default.aspx'
+var linkSelector = 'a#lnkBtnLogin';
 
 
 casper.start(address, function() {
-    this.fill('form[name="Form1"]', {
+    this.wait(2000, function(){
+        this.echo('Waited');
+    });
+});
+
+casper.then( function() {
+    this.sendKeys('input#Login1_txtMSISDN', user, {keepFocus: true});
+    this.sendKeys('input#Login1_txtPassword', pw, {keepFocus: true});
+    this.sendKeys('input#Login1_txtPassword', casper.page.event.key.Enter, {keepFocus: true});
+    
+    
+    // this.fill('form[name="Form1"]', {
+    this.fill('form#Form1', {
            'Login1:txtMSISDN': user,
            'Login1:txtPassword':pw,
-       }, true);
-       this.mouseEvent('click', '#lnkBtnLogin');
-});
+            }, true);
 
-
-casper.then(function() {
+    this.evaluate(function() {
+        document.querySelector('form#Form1').submit();
+    });
+    
+    // //
+    // // this.evaluate(function() {
+    // //     document.querySelector('a#lnkBtnLogin').classContent = 'primaryStandardBtn';
+    // // });
+    // this.evaluate(function(linkSelector) {
+    //         __utils__.findOne(linkSelector).setAttribute("class", "primaryStandardBtn");
+    //     }, linkSelector);
+    // // this.echo('Title is now: ' + this.evaluate(function() {
+    // //     return document.querySelector('div.div-login-button');
+    // // }));
+    // this.echo(this.getHTML('div.div-login-button'));
+    // // this.echo('Title is now: ' + this.evaluate(function() {
+    // //     return document.querySelector('h1').textContent;
+    // // }));
+    //
     this.wait(10000, function(){
-        this.capture(dir+'/data/bandwidth/tmobile_login.png');
-    })
+        this.mouseEvent('click', '#lnkBtnLogin');
+    });
+    this.capture(dir+'/data/bandwidth/tmobile_login.png');
+    
 });
 
 
-casper.then(function() {
-    try {
-        this.wait(2000, function() {
-            this.capture(dir+'/data/bandwidth/tmobile_page.png');
-            fs.write(bwlog, Date.now()+' : '+this.getHTML('div[class="topPanel"]')+'\n', 'a');
-        });
-    } catch(error) {
-        console.log(error);
-    }
-});
+
+// casper.then(function() {
+//     this.wait(10000, function(){
+//         this.capture(dir+'/data/bandwidth/tmobile_login.png');
+//     })
+// });
+
+//
+// casper.then(function() {
+//     try {
+//         this.wait(2000, function() {
+//             this.capture(dir+'/data/bandwidth/tmobile_page.png');
+//             fs.write(bwlog, Date.now()+' : '+this.getHTML('div[class="topPanel"]')+'\n', 'a');
+//         });
+//     } catch(error) {
+//         console.log(error);
+//     }
+// });
 
 casper.run(function () {
     this.echo('Done!').exit();
